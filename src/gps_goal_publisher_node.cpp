a665bb487f9a047623c08dd69c70a82bb9a070ef
Sudeep Fadadu <fadadusudeep93@gmail.com>
2014-11-08 02:17:58 +0530

    First commit Contains file read have not tested yet based on bool next gps point have to include subscriber for iterative gps goal publishing

---
+++ src/gps_goal_publisher_node.cpp
@@ -0,0 +1,42 @@
+#include <ros/ros.h>
+#include <sensor_msgs/NavSatStatus.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <std_msgs/Bool.h>
+
+ros::Publisher goal_publisher;
+bool next = true;
+ros::Rate loop_rate(4);
+
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "gps_goal_publisher");
+	ros::NodeHandle n;
+	goal_publisher = n.advertise<sensor_msgs::NavSatFix>("gps_goal", 10);
+	sensor_msgs::NavSatFix msg;
+	msg.header.stamp = ros::Time::now();
+	double gpsPoint[2];
+	ifstream inputFile("~/gps_waypoints.txt");
+	while(ros::ok())
+	{
+		if(next==true)
+		{
+			next = false;
+			if (inputFile) 
+			{        
+			    double value;
+			    inputFile >> value;
+			    gpsPoint[0]=(value);
+			    inputFile >> value;
+			    gpsPoint[1]=(value);
+			}
+
+		}
+		msg.latitude = gpsPoint[0];
+		msg.longitude =  gpsPoint[1];
+		goal_publisher.publish(msg);
+		ros::spinOnce();
+	    loop_rate.sleep();
+	}
+	return 0;
+}
